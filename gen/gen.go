9c4e01800e8abd7e2e01c1b347ee3c9e7244d04a
frrist <forrest@protocol.ai>
2020-01-21 14:49:31 -0800

    draft: add cbor gen to multisig actor

---
+++ gen/gen.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	multisig "github.com/filecoin-project/specs-actors/actors/builtin/multisig"
+	gen "github.com/whyrusleeping/cbor-gen"
+)
+
+func main() {
+
+	// General Types
+	if err := gen.WriteTupleEncodersToFile("./actors/builtin/multisig/cbor_gen.go", "multisig",
+		// actor state
+		multisig.MultiSigActorState{},
+		multisig.MultiSigTransaction{},
+		// method params
+		multisig.ConstructorParams{},
+		multisig.ProposeParams{},
+		multisig.TxnIDParams{},
+		multisig.ChangeNumApprovalsThresholdParams{},
+		multisig.SwapAuthorizedPartyParams{},
+		// method returns
+		multisig.ProposeReturn{},
+	); err != nil {
+		panic(err)
+	}
+}
