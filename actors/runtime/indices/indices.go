820bbf989a46c7760f97bebf3c3ee3ab3111cca4
Henri S <sternhenri@users.noreply.github.com>
2020-02-04 15:47:06 -0500

    comment out stale msg

--- actors/runtime/indices/indices.go
+++ actors/runtime/indices/indices.go
@@ -354,7 +354,7 @@ func ConsensusFault_SlasherShareGrowthRateDenom() big.Int {
 }
 
 func PaymentChannel_PaymentChannelSettleDelay() abi.ChainEpoch {
-	PARAM_FINISH()
+	// PARAM_FINISH
 	const PAYMENT_CHANNEL_SETTLE_DELAY = abi.ChainEpoch(1) // placeholder
 	return PAYMENT_CHANNEL_SETTLE_DELAY
 }
