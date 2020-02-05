366a47467b940cb34bfcde5bfe84aeee81cca7ce
anorth <anorth@users.noreply.github.com>
2020-02-05 08:31:55 -0800

    cbor

--- actors/builtin/storage_market/cbor_gen.go
+++ actors/builtin/storage_market/cbor_gen.go
@@ -857,6 +857,99 @@ func (t *GetPieceInfosForDealIDsReturn) UnmarshalCBOR(r io.Reader) error {
 	return nil
 }
 
+func (t *PublishStorageDealsReturn) MarshalCBOR(w io.Writer) error {
+	if t == nil {
+		_, err := w.Write(cbg.CborNull)
+		return err
+	}
+	if _, err := w.Write([]byte{129}); err != nil {
+		return err
+	}
+
+	// t.IDs ([]abi.DealID) (slice)
+	if len(t.IDs) > cbg.MaxLength {
+		return xerrors.Errorf("Slice value in field t.IDs was too long")
+	}
+
+	if _, err := w.Write(cbg.CborEncodeMajorType(cbg.MajArray, uint64(len(t.IDs)))); err != nil {
+		return err
+	}
+	for _, v := range t.IDs {
+		if v >= 0 {
+			if _, err := w.Write(cbg.CborEncodeMajorType(cbg.MajUnsignedInt, uint64(v))); err != nil {
+				return err
+			}
+		} else {
+			if _, err := w.Write(cbg.CborEncodeMajorType(cbg.MajNegativeInt, uint64(-v)-1)); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (t *PublishStorageDealsReturn) UnmarshalCBOR(r io.Reader) error {
+	br := cbg.GetPeeker(r)
+
+	maj, extra, err := cbg.CborReadHeader(br)
+	if err != nil {
+		return err
+	}
+	if maj != cbg.MajArray {
+		return fmt.Errorf("cbor input should be of type array")
+	}
+
+	if extra != 1 {
+		return fmt.Errorf("cbor input had wrong number of fields")
+	}
+
+	// t.IDs ([]abi.DealID) (slice)
+
+	maj, extra, err = cbg.CborReadHeader(br)
+	if err != nil {
+		return err
+	}
+
+	if extra > cbg.MaxLength {
+		return fmt.Errorf("t.IDs: array too large (%d)", extra)
+	}
+
+	if maj != cbg.MajArray {
+		return fmt.Errorf("expected cbor array")
+	}
+	if extra > 0 {
+		t.IDs = make([]abi.DealID, extra)
+	}
+	for i := 0; i < int(extra); i++ {
+		{
+			maj, extra, err := cbg.CborReadHeader(br)
+			var extraI int64
+			if err != nil {
+				return err
+			}
+			switch maj {
+			case cbg.MajUnsignedInt:
+				extraI = int64(extra)
+				if extraI < 0 {
+					return fmt.Errorf("int64 positive overflow")
+				}
+			case cbg.MajNegativeInt:
+				extraI = int64(extra)
+				if extraI < 0 {
+					return fmt.Errorf("int64 negative oveflow")
+				}
+				extraI = -1 - extraI
+			default:
+				return fmt.Errorf("wrong type for int64 field: %d", maj)
+			}
+
+			t.IDs[i] = abi.DealID(extraI)
+		}
+	}
+
+	return nil
+}
+
 func (t *StorageDealProposal) MarshalCBOR(w io.Writer) error {
 	if t == nil {
 		_, err := w.Write(cbg.CborNull)
