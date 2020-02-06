fc47c8d3ca4e1a96c02f6c32696638a2eadb4eb5
Frrist <forrest@protocol.ai>
2020-02-06 10:55:02 -0800

    test: account actor unit test (#123)
    * test: add address testing util methods
    * test: add account actor tests

--- support/testing/address.go
+++ support/testing/address.go
@@ -1,6 +1,7 @@
 package testing
 
 import (
+	"math/rand"
 	"testing"
 
 	addr "github.com/filecoin-project/go-address"
@@ -8,6 +9,36 @@ import (
 
 func NewIDAddr(t *testing.T, id uint64) addr.Address {
 	address, err := addr.NewIDAddress(id)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return address
+}
+
+func NewSECP256K1Addr(t *testing.T, pubkey string) addr.Address {
+	// the pubkey of a secp256k1 address is hashed for consistent length.
+	address, err := addr.NewSecp256k1Address([]byte(pubkey))
+	if err != nil {
+		t.Fatal(err)
+	}
+	return address
+}
+
+func NewBLSAddr(t *testing.T, seed int64) addr.Address {
+	// the pubkey of a bls address is not hashed and must be the correct length.
+	buf := make([]byte, 48)
+	r := rand.New(rand.NewSource(seed))
+	r.Read(buf)
+
+	address, err := addr.NewBLSAddress(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return address
+}
+
+func NewActorAddr(t *testing.T, data string) addr.Address {
+	address, err := addr.NewActorAddress([]byte(data))
 	if err != nil {
 		t.Fatal(err)
 	}
