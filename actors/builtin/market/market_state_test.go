30c1c88f0d45f98c49ad68c1713bd4974c703acd
Ingar Shu <ingar@users.noreply.github.com>
2020-02-11 14:20:55 -0800

    Initialize Market State fields (#152)
    * Initialize state fields
    
    - Initialize EscrowTable, LockedTable as maps, States as array
    
    * Assert that initial states of fields are expected values

---
+++ actors/builtin/market/market_state_test.go
@@ -0,0 +1,42 @@
+package market_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/filecoin-project/go-address"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/filecoin-project/specs-actors/actors/abi"
+	"github.com/filecoin-project/specs-actors/actors/builtin/market"
+	"github.com/filecoin-project/specs-actors/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/support/mock"
+)
+
+func TestConstruction(t *testing.T) {
+	builder := mock.NewBuilder(context.Background(), address.Undef)
+
+	t.Run("initializes all fields", func(t *testing.T) {
+		rt := builder.Build(t)
+		store := adt.AsStore(rt)
+
+		emptyMap, err := adt.MakeEmptyMap(store)
+		assert.NoError(t, err)
+
+		emptyArray, err := adt.MakeEmptyArray(store)
+		assert.NoError(t, err)
+
+		emptyMultiMap, err := market.MakeEmptySetMultimap(store)
+		assert.NoError(t, err)
+
+		state, err := market.ConstructState(store)
+		assert.NoError(t, err)
+
+		assert.Equal(t, emptyArray.Root(), state.Proposals)
+		assert.Equal(t, emptyArray.Root(), state.States)
+		assert.Equal(t, emptyMap.Root(), state.EscrowTable)
+		assert.Equal(t, emptyMap.Root(), state.LockedTable)
+		assert.Equal(t, abi.DealID(0), state.NextID)
+		assert.Equal(t, emptyMultiMap.Root(), state.DealIDsByParty)
+	})
+}
