73c232aae190055600d60b506e8c574859368a2d
Ignacio Corderi <icorderi@msn.com>
2020-02-19 12:40:52 -0800

    fix: ResolveAddress does not always return an ID address (#185)
    Co-authored-by: Alex North <445306+anorth@users.noreply.github.com>

--- actors/builtin/init/init_actor_state.go
+++ actors/builtin/init/init_actor_state.go
@@ -33,10 +33,19 @@ func ConstructState(store adt.Store, networkName string) (*State, error) {
 	}, nil
 }
 
-// Resolves an address to an ID-address, if possible.
-// If the provided address is not in the table, it is returned as-is. This means that ID-addresses
-// (which should only appear as values, not keys) and singleton actor addresses pass through unchanged.
+// ResolveAddress resolves an address to an ID-address, if possible.
+// If the provided address is an ID address, it is returned as-is.
+// This means that ID-addresses (which should only appear as values, not keys) and singleton actor addresses
+// pass through unchanged.
+//
+// Post-condition: all addresses succesfully returned by this method satisfy `addr.Protocol() == addr.ID`.
 func (s *State) ResolveAddress(store adt.Store, address addr.Address) (addr.Address, error) {
+	// Short-circuit ID address resolution.
+	if address.Protocol() == addr.ID {
+		return address, nil
+	}
+
+	// Lookup address.
 	m := adt.AsMap(store, s.AddressMap)
 	var actorID cbg.CborInt
 	found, err := m.Get(AddrKey(address), &actorID)
@@ -44,11 +53,14 @@ func (s *State) ResolveAddress(store adt.Store, address addr.Address) (addr.Addr
 		return addr.Undef, errors.Wrapf(err, "resolve address failed to look up map")
 	}
 	if found {
+		// Reconstruct address from the ActorID.
 		idAddr, err2 := addr.NewIDAddress(uint64(actorID))
 		autil.Assert(err2 == nil)
 		return idAddr, nil
 	}
-	return address, nil
+
+	// Not found.
+	return addr.Undef, errors.New("address not found")
 }
 
 // Allocates a new ID address and stores a mapping of the argument address to it.
