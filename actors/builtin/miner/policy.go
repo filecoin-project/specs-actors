103180bfddf16954b38300dde84c304eeba297c1
Erin Swenson-Healey <l@s3r.me>
2020-02-25 15:50:05 -0800

    update RegisteredProof values to conform v23 supported sector sizes (#203)
    * update RegisteredProof values to conform to v23 proofs
    
    * remove WinStacked as per PR feedback

--- actors/builtin/miner/policy.go
+++ actors/builtin/miner/policy.go
@@ -12,8 +12,7 @@ const ChainFinalityish = abi.ChainEpoch(500) // PARAM_FINISH
 // Maximum duration to allow for the sealing process for seal algorithms.
 // Dependent on algorithm and sector size
 var MaxSealDuration = map[abi.RegisteredProof]abi.ChainEpoch{
-	abi.RegisteredProof_StackedDRG32GiBSeal:    abi.ChainEpoch(1), // PARAM_FINISH
-	abi.RegisteredProof_WinStackedDRG32GiBSeal: abi.ChainEpoch(1), // PARAM_FINISH
+	abi.RegisteredProof_StackedDRG32GiBSeal: abi.ChainEpoch(1), // PARAM_FINISH
 }
 
 // Number of epochs between publishing the precommit and when the challenge for interactive PoRep is drawn
