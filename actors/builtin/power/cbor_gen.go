f5d1be7279929429574eb309178d8ea7c4878dff
Whyrusleeping <why@ipfs.io>
2020-02-26 14:24:40 -0800

    readdr miner owner param to createminer call (#208)

--- actors/builtin/power/cbor_gen.go
+++ actors/builtin/power/cbor_gen.go
@@ -378,7 +378,12 @@ func (t *CreateMinerParams) MarshalCBOR(w io.Writer) error {
 		_, err := w.Write(cbg.CborNull)
 		return err
 	}
-	if _, err := w.Write([]byte{131}); err != nil {
+	if _, err := w.Write([]byte{132}); err != nil {
+		return err
+	}
+
+	// t.Owner (address.Address) (struct)
+	if err := t.Owner.MarshalCBOR(w); err != nil {
 		return err
 	}
 
@@ -417,10 +422,19 @@ func (t *CreateMinerParams) UnmarshalCBOR(r io.Reader) error {
 		return fmt.Errorf("cbor input should be of type array")
 	}
 
-	if extra != 3 {
+	if extra != 4 {
 		return fmt.Errorf("cbor input had wrong number of fields")
 	}
 
+	// t.Owner (address.Address) (struct)
+
+	{
+
+		if err := t.Owner.UnmarshalCBOR(br); err != nil {
+			return err
+		}
+
+	}
 	// t.Worker (address.Address) (struct)
 
 	{
