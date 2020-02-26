59eacb5b76eaac29ffdd27b32515cbf010488ceb
Frrist <forrest@protocol.ai>
2020-02-26 14:57:03 -0800

    correct assertion in reward actor (#209)
    * add ExpectAssertionFailure method to mock runtime
    * fix assertion in reward actor and add test

--- actors/builtin/reward/reward_test.go
+++ actors/builtin/reward/reward_test.go
@@ -1,1 +1,70 @@
 package reward_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/filecoin-project/specs-actors/actors/abi"
+	"github.com/filecoin-project/specs-actors/actors/abi/big"
+	"github.com/filecoin-project/specs-actors/actors/builtin"
+	"github.com/filecoin-project/specs-actors/actors/builtin/reward"
+	"github.com/filecoin-project/specs-actors/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/support/mock"
+	tutil "github.com/filecoin-project/specs-actors/support/testing"
+)
+
+func TestConstructor(t *testing.T) {
+	actor := rewardHarness{reward.Actor{}, t}
+
+	rt := mock.NewBuilder(context.Background(), builtin.RewardActorAddr).
+		WithCaller(builtin.SystemActorAddr, builtin.SystemActorCodeID).
+		Build(t)
+
+	actor.constructAndVerify(rt)
+}
+
+func TestAwardBlockReward(t *testing.T) {
+	actor := rewardHarness{reward.Actor{}, t}
+	builder := mock.NewBuilder(context.Background(), builtin.RewardActorAddr).
+		WithCaller(builtin.SystemActorAddr, builtin.SystemActorCodeID)
+
+	t.Run("assertion failure when current balance is less than gas reward", func(t *testing.T) {
+		rt := builder.Build(t)
+		actor.constructAndVerify(rt)
+		miner := tutil.NewIDAddr(t, 1000)
+
+		gasreward := abi.NewTokenAmount(10)
+		rt.SetBalance(abi.NewTokenAmount(0))
+
+		rt.ExpectValidateCallerAddr(builtin.SystemActorAddr)
+		rt.ExpectAssertionFailure("actor current balance 0 insufficient to pay gas reward 10", func() {
+			rt.Call(actor.AwardBlockReward, &reward.AwardBlockRewardParams{
+				Miner:        miner,
+				Penalty:      big.Zero(),
+				GasReward:    gasreward,
+				NominalPower: big.Zero(),
+			})
+		})
+		rt.Verify()
+	})
+}
+
+type rewardHarness struct {
+	reward.Actor
+	t testing.TB
+}
+
+func (h *rewardHarness) constructAndVerify(rt *mock.Runtime) {
+	rt.ExpectValidateCallerAddr(builtin.SystemActorAddr)
+	ret := rt.Call(h.Constructor, &adt.EmptyValue{})
+	assert.Equal(h.t, &adt.EmptyValue{}, ret)
+	rt.Verify()
+
+	var st reward.State
+	rt.GetState(&st)
+	emptyMap := adt.AsMultimap(adt.AsStore(rt), st.RewardMap)
+	assert.Equal(h.t, emptyMap.Root(), st.RewardMap)
+	assert.Equal(h.t, big.Zero(), st.RewardTotal)
+}
