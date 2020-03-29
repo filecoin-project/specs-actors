244d870eb39238a1dd808f1624f9397caf26a11d
austinabell <austinabell8@gmail.com>
2020-03-29 12:26:33 -0400

    Update amount vested function

--- actors/builtin/reward/reward_state.go
+++ actors/builtin/reward/reward_state.go
@@ -1,6 +1,8 @@
 package reward
 
 import (
+	"fmt"
+
 	addr "github.com/filecoin-project/go-address"
 	cid "github.com/ipfs/go-cid"
 	errors "github.com/pkg/errors"
@@ -103,20 +105,23 @@ func (st *State) withdrawReward(store adt.Store, owner addr.Address, currEpoch a
 	return withdrawableSum, nil
 }
 
+// AmountVested returns the `TokenAmount` value of funds vested in the reward at an epoch
 func (r *Reward) AmountVested(currEpoch abi.ChainEpoch) abi.TokenAmount {
-	elapsed := currEpoch - r.StartEpoch
 	switch r.VestingFunction {
 	case None:
 		return r.Value
 	case Linear:
-		vestDuration := big.Sub(big.NewInt(int64(r.EndEpoch)), big.NewInt(int64(r.StartEpoch)))
-		if big.NewInt(int64(elapsed)).GreaterThanEqual(vestDuration) {
+		elapsed := currEpoch - r.StartEpoch
+
+		vestDuration := r.EndEpoch - r.StartEpoch
+		if elapsed >= vestDuration {
 			return r.Value
 		}
 
 		// totalReward * elapsedEpoch / vestDuration
-		return big.Div(big.Mul(r.Value, big.NewInt(int64(elapsed))), vestDuration)
+		return big.Mul(r.Value, big.NewInt(int64(elapsed/vestDuration)))
 	default:
-		return abi.NewTokenAmount(0)
+		panic(fmt.Sprintf("invalid vesting function %v", r.VestingFunction))
+
 	}
 }
