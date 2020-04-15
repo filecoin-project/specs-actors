cd367fabc5edeb4c07c7e5fb6a4e868b548aeb17
Henri S <sternhenri@users.noreply.github.com>
2020-04-14 22:52:46 -0400

    update consensusFault method

--- actors/builtin/miner/miner_actor.go
+++ actors/builtin/miner/miner_actor.go
@@ -681,7 +681,7 @@ func (a Actor) ReportConsensusFault(rt Runtime, params *ReportConsensusFaultPara
 	currEpoch := rt.CurrEpoch()
 	earliest := currEpoch - ConsensusFaultReportingWindow
 
-	fault, err := rt.Syscalls().VerifyConsensusFault(params.BlockHeader1, params.BlockHeader2, params.BlockHeaderExtra, earliest)
+	fault, err := rt.Syscalls().VerifyConsensusFault(params.BlockHeader1, params.BlockHeader2, params.BlockHeaderExtra)
 	if err != nil {
 		rt.Abortf(exitcode.ErrIllegalArgument, "fault not verified: %s", err)
 	}
