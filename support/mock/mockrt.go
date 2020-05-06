333184fb277f6e10326ffca71225c59b7812d1c8
anorth <445306+anorth@users.noreply.github.com>
2020-05-06 15:14:40 +1000

    Unit tests for prove commit sector

--- support/mock/mockrt.go
+++ support/mock/mockrt.go
@@ -56,6 +56,7 @@ type Runtime struct {
 	expectSends              []*expectedMessage
 	expectCreateActor        *expectCreateActor
 	expectVerifySig          *expectVerifySig
+	expectVerifySeal         *expectVerifySeal
 }
 
 type expectRandomness struct {
@@ -85,6 +86,11 @@ type expectVerifySig struct {
 	signer    addr.Address
 	plaintext []byte
 	// Result
+	result error
+}
+
+type expectVerifySeal struct {
+	seal   abi.SealVerifyInfo
 	result error
 }
 
@@ -210,8 +216,8 @@ func (rt *Runtime) GetRandomness(tag crypto.DomainSeparationTag, epoch abi.Chain
 	}
 	exp := rt.expectRandomness[0]
 	if tag != exp.tag || epoch != exp.epoch || !bytes.Equal(entropy, exp.entropy) {
-		rt.failTest("unexpected get randomness\n" +
-			"         tag: %d, epoch: %d, entropy: %v\n" +
+		rt.failTest("unexpected get randomness\n"+
+			"         tag: %d, epoch: %d, entropy: %v\n"+
 			"expected tag: %d, epoch: %d, entropy: %v", tag, epoch, entropy, exp.tag, exp.epoch, exp.entropy)
 	}
 	defer func() {
@@ -409,8 +415,8 @@ func (rt *Runtime) VerifySignature(sig crypto.Signature, signer addr.Address, pl
 	exp := rt.expectVerifySig
 	if exp != nil {
 		if !exp.sig.Equals(&sig) || exp.signer != signer || !bytes.Equal(exp.plaintext, plaintext) {
-			rt.failTest("unexpected signature verification\n" +
-				"         sig: %v, signer: %s, plaintext: %v\n" +
+			rt.failTest("unexpected signature verification\n"+
+				"         sig: %v, signer: %s, plaintext: %v\n"+
 				"expected sig: %v, signer: %s, plaintext: %v",
 				sig, signer, plaintext, exp.sig, exp.signer, exp.plaintext)
 		}
@@ -431,8 +437,22 @@ func (rt *Runtime) ComputeUnsealedSectorCID(reg abi.RegisteredProof, pieces []ab
 	panic("implement me")
 }
 
-func (rt *Runtime) VerifySeal(vi abi.SealVerifyInfo) error {
-	panic("implement me")
+func (rt *Runtime) VerifySeal(seal abi.SealVerifyInfo) error {
+	exp := rt.expectVerifySeal
+	if exp != nil {
+		if !reflect.DeepEqual(exp.seal, seal) {
+			rt.failTest("unexpected seal verification\n"+
+				"        : %v\n"+
+				"expected: %v",
+				seal, exp.seal)
+		}
+		defer func() {
+			rt.expectVerifySeal = nil
+		}()
+		return exp.result
+	}
+	rt.failTestNow("unexpected syscall to verify seal %v", seal)
+	return nil
 }
 
 func (rt *Runtime) VerifyPoSt(vi abi.WindowPoStVerifyInfo) error {
@@ -571,6 +591,12 @@ func (rt *Runtime) ExpectVerifySignature(sig crypto.Signature, signer addr.Addre
 	}
 }
 
+func (rt *Runtime) ExpectVerifySeal(seal abi.SealVerifyInfo, result error) {
+	rt.expectVerifySeal = &expectVerifySeal{
+		seal:   seal,
+		result: result,
+	}
+}
 
 // Verifies that expected calls were received, and resets all expectations.
 func (rt *Runtime) Verify() {
@@ -584,11 +610,18 @@ func (rt *Runtime) Verify() {
 		rt.failTest("missing expected ValidateCallerType %v", rt.expectValidateCallerType)
 	}
 	if len(rt.expectSends) > 0 {
-		rt.failTest("missing expected Send: %v", rt.expectSends)
+		rt.failTest("missing expected send %v", rt.expectSends)
 	}
 	if rt.expectCreateActor != nil {
-		rt.failTest("missing expected create actor with code: %s, address %s",
+		rt.failTest("missing expected create actor with code %s, address %s",
 			rt.expectCreateActor.codeId, rt.expectCreateActor.address)
+	}
+	if rt.expectVerifySig != nil {
+		rt.failTest("missing expected verify signature with signer %s, sig %v, plaintext %v",
+			rt.expectVerifySig.signer, rt.expectVerifySig.sig, rt.expectVerifySig.plaintext)
+	}
+	if rt.expectVerifySeal != nil {
+		rt.failTest("missing expected verify seal with %v", rt.expectVerifySeal.seal)
 	}
 
 	rt.Reset()
@@ -599,7 +632,10 @@ func (rt *Runtime) Reset() {
 	rt.expectValidateCallerAny = false
 	rt.expectValidateCallerAddr = nil
 	rt.expectValidateCallerType = nil
+	rt.expectSends = nil
 	rt.expectCreateActor = nil
+	rt.expectVerifySig = nil
+	rt.expectVerifySeal = nil
 }
 
 // Calls f() expecting it to invoke Runtime.Abortf() with a specified exit code.
