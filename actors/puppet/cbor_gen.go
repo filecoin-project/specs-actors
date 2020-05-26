acfd10031eeffcdbb9126793428613149101ce0e
laser <l@s3r.me>
2020-05-26 09:16:09 -0700

    add PutMarshalCBORFailure method, to test CBOR marshaling issues

--- actors/puppet/cbor_gen.go
+++ actors/puppet/cbor_gen.go
@@ -19,8 +19,22 @@ func (t *State) MarshalCBOR(w io.Writer) error {
 		_, err := w.Write(cbg.CborNull)
 		return err
 	}
-	if _, err := w.Write([]byte{128}); err != nil {
+	if _, err := w.Write([]byte{129}); err != nil {
 		return err
+	}
+
+	// t.SomeField ([]*puppet.FailToMarshalCBOR) (slice)
+	if len(t.OptFailToMarshalCBOR) > cbg.MaxLength {
+		return xerrors.Errorf("Slice value in field t.SomeField was too long")
+	}
+
+	if _, err := w.Write(cbg.CborEncodeMajorType(cbg.MajArray, uint64(len(t.OptFailToMarshalCBOR)))); err != nil {
+		return err
+	}
+	for _, v := range t.OptFailToMarshalCBOR {
+		if err := v.MarshalCBOR(w); err != nil {
+			return err
+		}
 	}
 	return nil
 }
@@ -36,8 +50,37 @@ func (t *State) UnmarshalCBOR(r io.Reader) error {
 		return fmt.Errorf("cbor input should be of type array")
 	}
 
-	if extra != 0 {
+	if extra != 1 {
 		return fmt.Errorf("cbor input had wrong number of fields")
+	}
+
+	// t.SomeField ([]*puppet.FailToMarshalCBOR) (slice)
+
+	maj, extra, err = cbg.CborReadHeader(br)
+	if err != nil {
+		return err
+	}
+
+	if extra > cbg.MaxLength {
+		return fmt.Errorf("t.SomeField: array too large (%d)", extra)
+	}
+
+	if maj != cbg.MajArray {
+		return fmt.Errorf("expected cbor array")
+	}
+
+	if extra > 0 {
+		t.OptFailToMarshalCBOR = make([]*FailToMarshalCBOR, extra)
+	}
+
+	for i := 0; i < int(extra); i++ {
+
+		var v FailToMarshalCBOR
+		if err := v.UnmarshalCBOR(br); err != nil {
+			return err
+		}
+
+		t.OptFailToMarshalCBOR[i] = &v
 	}
 
 	return nil
