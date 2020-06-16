d4f262324ec6220238ee50376995a14c65f98946
acruikshank <acruikshank@example.com>
2020-06-16 16:17:12 -0400

    normalize ExpectedDayRewardForSector with InitialPldgeForPower and use former in latter

--- actors/builtin/miner/monies.go
+++ actors/builtin/miner/monies.go
@@ -3,7 +3,18 @@ package miner
 import (
 	"github.com/filecoin-project/specs-actors/actors/abi"
 	"github.com/filecoin-project/specs-actors/actors/abi/big"
+	"github.com/filecoin-project/specs-actors/actors/builtin"
+	"github.com/filecoin-project/specs-actors/actors/util"
 )
+
+// This is the BR(t) value of the given sector for the current epoch.
+// It is the expected reward this sector would pay out over a one day period.
+// BR(t) = CurrEpochReward(t) * SectorQualityAdjustedPower * EpochsInDay / TotalNetworkQualityAdjustedPower(t)
+func ExpectedDayRewardForSector(epochTargetReward abi.TokenAmount, networkQAPower abi.StoragePower, qaSectorPower abi.StoragePower) abi.TokenAmount {
+	util.Assert(!networkQAPower.IsZero())
+	expectedRewardForProvingPeriod := big.Mul(big.NewInt(builtin.EpochsInDay), epochTargetReward)
+	return big.Div(big.Mul(qaSectorPower, expectedRewardForProvingPeriod), networkQAPower)
+}
 
 // Computes the pledge requirement for committing new quality-adjusted power to the network, given the current
 // total power, total pledge commitment, epoch block reward, and circulating token supply.
@@ -11,7 +22,7 @@ import (
 // newly-committed power, holding the per-epoch block reward constant (though in reality it will change over time).
 // The network total pledge and circulating supply parameters are currently unused, but may be included in a
 // future calculation.
-func InitialPledgeForPower(newQAPower abi.StoragePower, networkQAPower abi.StoragePower, networkTotalPledge abi.TokenAmount, epochTargetReward abi.TokenAmount, networkCirculatingSupply abi.TokenAmount) abi.TokenAmount {
+func InitialPledgeForPower(qaSectorPower abi.StoragePower, networkQAPower abi.StoragePower, networkTotalPledge abi.TokenAmount, epochTargetReward abi.TokenAmount, networkCirculatingSupply abi.TokenAmount) abi.TokenAmount {
 	// Details here are still subject to change.
 	// PARAM_FINISH
 	// https://github.com/filecoin-project/specs-actors/issues/468
@@ -21,6 +32,5 @@ func InitialPledgeForPower(newQAPower abi.StoragePower, networkQAPower abi.Stora
 	if networkQAPower.IsZero() {
 		return epochTargetReward
 	}
-	return big.Div(big.Mul(newQAPower, epochTargetReward), networkQAPower)
+	return big.Mul(big.NewInt(20), ExpectedDayRewardForSector(epochTargetReward, networkQAPower, qaSectorPower))
 }
-
