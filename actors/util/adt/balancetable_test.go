2c177c5be5f4ad51d6e54245326ffa1fcc295adf
Aarsh Shah <aarshkshah1992@gmail.com>
2020-07-13 15:38:50 +0530

    balance table tests

--- actors/util/adt/balancetable_test.go
+++ actors/util/adt/balancetable_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/specs-actors/actors/abi"
 	"github.com/filecoin-project/specs-actors/actors/abi/big"
@@ -15,14 +16,19 @@ import (
 )
 
 func TestBalanceTable(t *testing.T) {
-	t.Run("AddCreate adds or creates", func(t *testing.T) {
-		addr := tutil.NewIDAddr(t, 100)
+	buildBalanceTable := func() *adt.BalanceTable {
 		rt := mock.NewBuilder(context.Background(), address.Undef).Build(t)
 		store := adt.AsStore(rt)
 		emptyMap := adt.MakeEmptyMap(store)
 
 		bt, err := adt.AsBalanceTable(store, tutil.MustRoot(t, emptyMap))
-		assert.NoError(t, err)
+		require.NoError(t, err)
+		return bt
+	}
+
+	t.Run("AddCreate adds or creates", func(t *testing.T) {
+		addr := tutil.NewIDAddr(t, 100)
+		bt := buildBalanceTable()
 
 		has, err := bt.Has(addr)
 		assert.NoError(t, err)
@@ -43,16 +49,69 @@ func TestBalanceTable(t *testing.T) {
 		assert.Equal(t, abi.NewTokenAmount(30), amount)
 	})
 
+	t.Run("Add works only if account has been created", func(t *testing.T) {
+		addr := tutil.NewIDAddr(t, 100)
+		bt := buildBalanceTable()
+
+		// fail if account hasnt been created
+		require.Error(t, bt.Add(addr, abi.NewTokenAmount(10)))
+
+		// now create account -> add should work
+		require.NoError(t, bt.AddCreate(addr, abi.NewTokenAmount(10)))
+		require.NoError(t, bt.Add(addr, abi.NewTokenAmount(5)))
+		bal, err := bt.Get(addr)
+		require.NoError(t, err)
+		require.Equal(t, abi.NewTokenAmount(15), bal)
+	})
+
+	t.Run("Get works only if account has been created", func(t *testing.T) {
+		addr := tutil.NewIDAddr(t, 100)
+		bt := buildBalanceTable()
+
+		// fail if account hasnt been created
+		bal, err := bt.Get(addr)
+		require.Error(t, err)
+		require.Equal(t, big.Zero(), bal)
+
+		// now create account -> add should work
+		require.NoError(t, bt.AddCreate(addr, abi.NewTokenAmount(10)))
+		bal, err = bt.Get(addr)
+		require.NoError(t, err)
+		require.Equal(t, abi.NewTokenAmount(10), bal)
+	})
+
+	t.Run("Must subtract fails if account not created or balance is insufficient", func(t *testing.T) {
+		addr := tutil.NewIDAddr(t, 100)
+		bt := buildBalanceTable()
+
+		// fail if account hasnt been created
+		require.Error(t, bt.MustSubtract(addr, abi.NewTokenAmount(0)))
+
+		// create account -> should work now
+		require.NoError(t, bt.AddCreate(addr, abi.NewTokenAmount(0)))
+		require.NoError(t, bt.MustSubtract(addr, abi.NewTokenAmount(0)))
+
+		require.NoError(t, bt.Add(addr, abi.NewTokenAmount(5)))
+		// error if insufficient balance -> however, balance is still subtracted
+		// TODO Is that correct ?
+		require.Error(t, bt.MustSubtract(addr, abi.NewTokenAmount(6)))
+		bal, err := bt.Get(addr)
+		require.NoError(t, err)
+		require.Equal(t, abi.NewTokenAmount(0), bal)
+
+		// balance is sufficient -> should work
+		require.NoError(t, bt.Add(addr, abi.NewTokenAmount(5)))
+		require.NoError(t, bt.MustSubtract(addr, abi.NewTokenAmount(4)))
+		bal, err = bt.Get(addr)
+		require.NoError(t, err)
+		require.Equal(t, abi.NewTokenAmount(1), bal)
+	})
+
 	t.Run("Total returns total amount tracked", func(t *testing.T) {
 		addr1 := tutil.NewIDAddr(t, 100)
 		addr2 := tutil.NewIDAddr(t, 101)
 
-		rt := mock.NewBuilder(context.Background(), address.Undef).Build(t)
-		store := adt.AsStore(rt)
-		emptyMap := adt.MakeEmptyMap(store)
-
-		bt, err := adt.AsBalanceTable(store, tutil.MustRoot(t, emptyMap))
-		assert.NoError(t, err)
+		bt := buildBalanceTable()
 		total, err := bt.Total()
 		assert.NoError(t, err)
 		assert.Equal(t, big.Zero(), total)
@@ -76,5 +135,44 @@ func TestBalanceTable(t *testing.T) {
 			assert.NoError(t, err)
 			assert.Equal(t, abi.NewTokenAmount(tc.total), total)
 		}
+	})
+}
+
+func TestSubtractWithMinimum(t *testing.T) {
+	buildBalanceTable := func() *adt.BalanceTable {
+		rt := mock.NewBuilder(context.Background(), address.Undef).Build(t)
+		store := adt.AsStore(rt)
+		emptyMap := adt.MakeEmptyMap(store)
+
+		bt, err := adt.AsBalanceTable(store, tutil.MustRoot(t, emptyMap))
+		require.NoError(t, err)
+		return bt
+	}
+	addr := tutil.NewIDAddr(t, 100)
+	zeroAmt := abi.NewTokenAmount(0)
+
+	t.Run("fails when account does not exist", func(t *testing.T) {
+		bt := buildBalanceTable()
+		s, err := bt.SubtractWithMinimum(addr, zeroAmt, zeroAmt)
+		require.Error(t, err)
+		require.EqualValues(t, zeroAmt, s)
+	})
+
+	t.Run("withdraw available when account does not have sufficient balance", func(t *testing.T) {
+		bt := buildBalanceTable()
+		require.NoError(t, bt.AddCreate(addr, abi.NewTokenAmount(5)))
+
+		s, err := bt.SubtractWithMinimum(addr, abi.NewTokenAmount(2), abi.NewTokenAmount(4))
+		require.NoError(t, err)
+		require.EqualValues(t, abi.NewTokenAmount(1), s)
+	})
+
+	t.Run("account has sufficient balance", func(t *testing.T) {
+		bt := buildBalanceTable()
+		require.NoError(t, bt.AddCreate(addr, abi.NewTokenAmount(5)))
+
+		s, err := bt.SubtractWithMinimum(addr, abi.NewTokenAmount(3), abi.NewTokenAmount(2))
+		require.NoError(t, err)
+		require.EqualValues(t, abi.NewTokenAmount(3), s)
 	})
 }
