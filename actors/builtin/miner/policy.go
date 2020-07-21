d06ea9cf6faa9b7fd7112f337552377690a1202b
Nicola <1424850+nicola@users.noreply.github.com>
2020-07-21 09:56:10 +0100

    Param Audit: SectorsMax clarification

--- actors/builtin/miner/policy.go
+++ actors/builtin/miner/policy.go
@@ -36,7 +36,7 @@ func init() {
 }
 
 // Filecoin Parameter: The maximum number of sectors that a miner can have (and implicitly the maximum number of faults, recoveries, etc.) 
-// Motivation: This guarantees that actors operations per miner are bounded. Also, this reflects the limit of sectors that can be proven in a day via WindowPoSt.
+// Motivation: This guarantees that actors operations per miner are bounded.
 // TODO raise this number, carefully
 // https://github.com/filecoin-project/specs-actors/issues/470
 const SectorsMax = 32 << 20 // PARAM_FINISH
