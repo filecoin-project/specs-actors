0c6d320efb473624f3af32c116c7a48667232144
Nicola <1424850+nicola@users.noreply.github.com>
2020-07-22 11:50:31 +0100

    Param Audit: ConsensusMinerMinMiners description

--- actors/builtin/power/policy.go
+++ actors/builtin/power/policy.go
@@ -5,8 +5,9 @@ import (
 )
 
 // PARAM_SPEC
-// Minimum number of registered miners for the minimum miner size limit to effectively limit consensus power.
-const ConsensusMinerMinMiners = 3
+// This is a switch for turning on the minimum miner size requirement.
+// When this number of miners of minimum miner size is reached, minimum miner size is enforce.
+const ConsensusMinerMinMiners = 4
 
 // Minimum power of an individual miner to meet the threshold for leader election.
 var ConsensusMinerMinPower = abi.NewStoragePower(1 << 40) // PARAM_FINISH
