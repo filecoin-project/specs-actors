463a14a0f39df77ed37a2fb1c2d58793e5e78937
anorth <445306+anorth@users.noreply.github.com>
2020-07-31 14:37:03 +1000

    AwardBlockReward validates params, better tests

--- actors/builtin/reward/reward_test.go
+++ actors/builtin/reward/reward_test.go
@@ -4,20 +4,24 @@ import (
 	"context"
 	"testing"
 
+	address "github.com/filecoin-project/go-address"
 	"github.com/stretchr/testify/assert"
 
-	address "github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/specs-actors/actors/abi"
 	"github.com/filecoin-project/specs-actors/actors/abi/big"
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/builtin/reward"
+	"github.com/filecoin-project/specs-actors/actors/runtime/exitcode"
 	"github.com/filecoin-project/specs-actors/support/mock"
 	tutil "github.com/filecoin-project/specs-actors/support/testing"
 )
 
+
 func TestExports(t *testing.T) {
 	mock.CheckActorExports(t, reward.Actor{})
 }
+
+const EpochZeroReward = "36266264293777134739"
 
 func TestConstructor(t *testing.T) {
 	actor := rewardHarness{reward.Actor{}, t}
@@ -31,7 +35,7 @@ func TestConstructor(t *testing.T) {
 		st := getState(rt)
 		assert.Equal(t, abi.ChainEpoch(0), st.Epoch)
 		assert.Equal(t, abi.NewStoragePower(0), st.CumsumRealized)
-		assert.Equal(t, big.MustFromString("36266264293777134739"), st.ThisEpochReward)
+		assert.Equal(t, big.MustFromString(EpochZeroReward), st.ThisEpochReward)
 		epochZeroBaseline := big.Sub(reward.BaselineInitialValue, big.NewInt(1)) // account for rounding error of one byte during construction
 		assert.Equal(t, epochZeroBaseline, st.ThisEpochBaselinePower)
 		assert.Equal(t, reward.BaselineInitialValue, st.EffectiveBaselinePower)
@@ -72,44 +76,85 @@ func TestConstructor(t *testing.T) {
 
 func TestAwardBlockReward(t *testing.T) {
 	actor := rewardHarness{reward.Actor{}, t}
+	winner := tutil.NewIDAddr(t, 1000)
 	builder := mock.NewBuilder(context.Background(), builtin.RewardActorAddr).
 		WithCaller(builtin.SystemActorAddr, builtin.SystemActorCodeID)
 
-	t.Run("assertion failure when current balance is less than gas reward", func(t *testing.T) {
+	t.Run("rejects gas reward exceeding balance", func(t *testing.T) {
 		rt := builder.Build(t)
 		startRealizedPower := abi.NewStoragePower(0)
 		actor.constructAndVerify(rt, &startRealizedPower)
-		miner := tutil.NewIDAddr(t, 1000)
 
-		gasreward := abi.NewTokenAmount(10)
-		rt.SetBalance(abi.NewTokenAmount(0))
-
+		rt.SetBalance(abi.NewTokenAmount(9))
 		rt.ExpectValidateCallerAddr(builtin.SystemActorAddr)
-		rt.ExpectAssertionFailure("actor current balance 0 insufficient to pay gas reward 10", func() {
-			rt.Call(actor.AwardBlockReward, &reward.AwardBlockRewardParams{
-				Miner:     miner,
-				Penalty:   big.Zero(),
-				GasReward: gasreward,
-				WinCount:  1,
-			})
+		rt.ExpectAbort(exitcode.ErrIllegalState, func() {
+			gasReward := big.NewInt(10)
+			actor.awardBlockReward(rt, winner, big.Zero(), gasReward, 1, big.Zero())
 		})
-		rt.Verify()
+	})
+
+	t.Run("rejects negative penalty or reward", func(t *testing.T) {
+		rt := builder.Build(t)
+		startRealizedPower := abi.NewStoragePower(0)
+		actor.constructAndVerify(rt, &startRealizedPower)
+
+		rt.SetBalance(abi.NewTokenAmount(1e18))
+		rt.ExpectValidateCallerAddr(builtin.SystemActorAddr)
+		rt.ExpectAbort(exitcode.ErrIllegalArgument, func() {
+			penalty := big.NewInt(-1)
+			actor.awardBlockReward(rt, winner, penalty, big.Zero(), 1, big.Zero())
+		})
+		rt.Reset()
+		rt.ExpectAbort(exitcode.ErrIllegalArgument, func() {
+			gasReward := big.NewInt(-1)
+			actor.awardBlockReward(rt, winner, big.Zero(), gasReward, 1, big.Zero())
+		})
+	})
+
+	t.Run("rejects zero wincount", func(t *testing.T) {
+		rt := builder.Build(t)
+		startRealizedPower := abi.NewStoragePower(0)
+		actor.constructAndVerify(rt, &startRealizedPower)
+
+		rt.SetBalance(abi.NewTokenAmount(1e18))
+		rt.ExpectValidateCallerAddr(builtin.SystemActorAddr)
+		rt.ExpectAbort(exitcode.ErrIllegalArgument, func() {
+			actor.awardBlockReward(rt, winner, big.Zero(), big.Zero(), 0, big.Zero())
+		})
+		rt.Reset()
+	})
+
+	t.Run("pays reward and burns penalty", func(t *testing.T) {
+		rt := builder.Build(t)
+		startRealizedPower := abi.NewStoragePower(0)
+		actor.constructAndVerify(rt, &startRealizedPower)
+
+		rt.SetBalance(big.Mul(big.NewInt(1e9), abi.NewTokenAmount(1e18)))
+		rt.ExpectValidateCallerAddr(builtin.SystemActorAddr)
+		penalty := big.NewInt(100)
+		gasReward := big.NewInt(200)
+		expectedReward := big.Sum(big.Div(big.MustFromString(EpochZeroReward), big.NewInt(5)), gasReward, penalty.Neg())
+		actor.awardBlockReward(rt, winner, penalty, gasReward, 1, expectedReward)
+		rt.Reset()
 	})
 
 	t.Run("pays out current balance when reward exceeds total balance", func(t *testing.T) {
 		rt := builder.Build(t)
 		startRealizedPower := abi.NewStoragePower(1)
 		actor.constructAndVerify(rt, &startRealizedPower)
-		miner := tutil.NewIDAddr(t, 1000)
 
 		// Total reward is a huge number, upon writing ~1e18, so 300 should be way less
 		smallReward := abi.NewTokenAmount(300)
+		penalty := abi.NewTokenAmount(100)
 		rt.SetBalance(smallReward)
 		rt.ExpectValidateCallerAddr(builtin.SystemActorAddr)
-		rt.ExpectSend(miner, builtin.MethodsMiner.AddLockedFund, &smallReward, smallReward, nil, 0)
+
+		expectedReward := big.Sub(smallReward, penalty)
+		rt.ExpectSend(winner, builtin.MethodsMiner.AddLockedFund, &expectedReward, expectedReward, nil, 0)
+		rt.ExpectSend(builtin.BurntFundsActorAddr, builtin.MethodSend, nil, penalty, nil, 0)
 		rt.Call(actor.AwardBlockReward, &reward.AwardBlockRewardParams{
-			Miner:     miner,
-			Penalty:   big.Zero(),
+			Miner:     winner,
+			Penalty:   penalty,
 			GasReward: big.Zero(),
 			WinCount:  1,
 		})
@@ -182,18 +227,18 @@ func (h *rewardHarness) updateNetworkKPI(rt *mock.Runtime, currRawPower *abi.Sto
 	rt.Verify()
 }
 
-func (h *rewardHarness) awardBlockReward(rt *mock.Runtime, miner address.Address, penalty, gasReward abi.TokenAmount, winCount int64, expectedTotalReward abi.TokenAmount) {
+func (h *rewardHarness) awardBlockReward(rt *mock.Runtime, miner address.Address, penalty, gasReward abi.TokenAmount, winCount int64, expectedPayment abi.TokenAmount) {
 	rt.ExpectValidateCallerAddr(builtin.SystemActorAddr)
-	rt.ExpectSend(miner, builtin.MethodsMiner.AddLockedFund, &expectedTotalReward, expectedTotalReward, nil, 0)
+	rt.ExpectSend(miner, builtin.MethodsMiner.AddLockedFund, &expectedPayment, expectedPayment, nil, 0)
+	if penalty.GreaterThan(big.Zero()) {
+		rt.ExpectSend(builtin.BurntFundsActorAddr, builtin.MethodSend, nil, penalty, nil, 0)
+	}
 	rt.Call(h.AwardBlockReward, &reward.AwardBlockRewardParams{
 		Miner:     miner,
-		Penalty:   big.Zero(),
-		GasReward: big.Zero(),
-		WinCount:  1,
+		Penalty:   penalty,
+		GasReward: gasReward,
+		WinCount:  winCount,
 	})
-	if penalty.GreaterThan(big.Zero()) {
-		rt.ExpectSend(builtin.BurntFundsActorAddr, builtin.MethodSend, &penalty, penalty, nil, 0)
-	}
 	rt.Verify()
 }
 
