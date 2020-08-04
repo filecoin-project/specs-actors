debb0650ec35d8143a6dfd7ed23beaf4cbf338f4
Steven Allen <steven@stebalien.com>
2020-08-04 11:52:11 -0700

    Remove runtime from partition/deadline state assertions (#879)
    * Remove runtime from partition/deadline state assertions
    * Remove runtime from state tests.

--- actors/builtin/miner/sectors_test.go
+++ actors/builtin/miner/sectors_test.go
@@ -5,12 +5,11 @@ import (
 
 	"github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
-	"github.com/filecoin-project/specs-actors/support/mock"
 	"github.com/stretchr/testify/require"
 )
 
-func sectorsArr(t *testing.T, rt *mock.Runtime, sectors []*miner.SectorOnChainInfo) miner.Sectors {
-	sectorArr := miner.Sectors{adt.MakeEmptyArray(adt.AsStore(rt))}
+func sectorsArr(t *testing.T, store adt.Store, sectors []*miner.SectorOnChainInfo) miner.Sectors {
+	sectorArr := miner.Sectors{adt.MakeEmptyArray(store)}
 	require.NoError(t, sectorArr.Store(sectors...))
 	return sectorArr
 }
