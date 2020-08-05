1b2d3a8a03392dc951e5a8df751302b497293cfb
Aarsh Shah <aarshkshah1992@gmail.com>
2020-08-05 17:33:50 +0530

    changes as per review

--- actors/builtin/miner/miner_state.go
+++ actors/builtin/miner/miner_state.go
@@ -11,10 +11,11 @@ import (
 	errors "github.com/pkg/errors"
 	xerrors "golang.org/x/xerrors"
 
+	. "github.com/filecoin-project/specs-actors/actors/util"
+
 	abi "github.com/filecoin-project/specs-actors/actors/abi"
 	big "github.com/filecoin-project/specs-actors/actors/abi/big"
 	xc "github.com/filecoin-project/specs-actors/actors/runtime/exitcode"
-	. "github.com/filecoin-project/specs-actors/actors/util"
 	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
 
@@ -32,7 +33,7 @@ type State struct {
 	PreCommitDeposits abi.TokenAmount // Total funds locked as PreCommitDeposits
 	LockedFunds       abi.TokenAmount // Total rewards and added funds locked in vesting table
 
-	VestingFunds cid.Cid
+	VestingFunds cid.Cid // Vesting Funds schedule for the miner. Slice[(VestingEpoch, VestingAmount)]
 
 	InitialPledgeRequirement abi.TokenAmount // Sum of initial pledge requirements of all active sectors
 
@@ -775,53 +776,15 @@ func (st *State) AddLockedFunds(store adt.Store, currEpoch abi.ChainEpoch, vesti
 	AssertMsg(vestingSum.GreaterThanEqual(big.Zero()), "negative vesting sum %s", vestingSum)
 
 	// unlock vested funds first
-	amountUnlocked := st.unlockVestedFunds(vestingFunds, currEpoch)
+	amountUnlocked := vestingFunds.unlockVestedFunds(currEpoch)
+	st.LockedFunds = big.Sub(st.LockedFunds, amountUnlocked)
+	Assert(st.LockedFunds.GreaterThanEqual(big.Zero()))
 
-	// maps the epochs in VestingFunds to their indices in the slice
-	epochToIndex := make(map[abi.ChainEpoch]int, len(vestingFunds.Funds))
-	for i, vf := range vestingFunds.Funds {
-		epochToIndex[vf.Epoch] = i
-	}
-
-	// Quantization is aligned with when regular cron will be invoked, in the last epoch of deadlines.
-	vestBegin := currEpoch + spec.InitialDelay // Nothing unlocks here, this is just the start of the clock.
-	vestPeriod := big.NewInt(int64(spec.VestPeriod))
-	vestedSoFar := big.Zero()
-	for e := vestBegin + spec.StepDuration; vestedSoFar.LessThan(vestingSum); e += spec.StepDuration {
-		vestEpoch := quantizeUp(e, spec.Quantization, st.ProvingPeriodStart)
-		elapsed := vestEpoch - vestBegin
-
-		targetVest := big.Zero() //nolint:ineffassign
-		if elapsed < spec.VestPeriod {
-			// Linear vesting, PARAM_FINISH
-			targetVest = big.Div(big.Mul(vestingSum, big.NewInt(int64(elapsed))), vestPeriod)
-		} else {
-			targetVest = vestingSum
-		}
-
-		vestThisTime := big.Sub(targetVest, vestedSoFar)
-		vestedSoFar = targetVest
-
-		// epoch already exists. Load existing entry
-		// and update amount.
-		if index, ok := epochToIndex[vestEpoch]; ok {
-			currentAmt := vestingFunds.Funds[index].Amount
-			vestingFunds.Funds[index].Amount = big.Add(currentAmt, vestThisTime)
-		} else {
-			// append a new entry -> slice will be sorted by epoch later.
-			entry := VestingFund{Epoch: vestEpoch, Amount: vestThisTime}
-			vestingFunds.Funds = append(vestingFunds.Funds, entry)
-			epochToIndex[vestEpoch] = len(vestingFunds.Funds) - 1
-		}
-	}
-
-	// sort slice by epoch
-	sort.Slice(vestingFunds.Funds, func(first, second int) bool {
-		return vestingFunds.Funds[first].Epoch < vestingFunds.Funds[second].Epoch
-	})
-
+	// add locked funds now
+	vestingFunds.addLockedFunds(currEpoch, vestingSum, st.ProvingPeriodStart, spec)
 	st.LockedFunds = big.Add(st.LockedFunds, vestingSum)
 
+	// save the updated vesting table state
 	if err := st.SaveVestingFunds(store, vestingFunds); err != nil {
 		return big.Zero(), xerrors.Errorf("failed to save vesting funds: %w", err)
 	}
@@ -860,31 +823,7 @@ func (st *State) UnlockUnvestedFunds(store adt.Store, currEpoch abi.ChainEpoch, 
 		return big.Zero(), xerrors.Errorf("failed tp load vesting funds: %w", err)
 	}
 
-	amountUnlocked := abi.NewTokenAmount(0)
-	lastIndexToRemove := -1
-
-	for i, vf := range vestingFunds.Funds {
-		if amountUnlocked.GreaterThanEqual(target) {
-			break
-		}
-
-		if vf.Epoch >= currEpoch {
-			unlockAmount := big.Min(big.Sub(target, amountUnlocked), vf.Amount)
-			amountUnlocked = big.Add(amountUnlocked, unlockAmount)
-			newAmount := big.Sub(vf.Amount, unlockAmount)
-
-			if newAmount.IsZero() {
-				lastIndexToRemove = i
-			} else {
-				vestingFunds.Funds[i].Amount = newAmount
-			}
-		}
-	}
-
-	// remove all entries upto and including lastIndexToRemove
-	if lastIndexToRemove != -1 {
-		vestingFunds.Funds = vestingFunds.Funds[lastIndexToRemove+1:]
-	}
+	amountUnlocked := vestingFunds.unlockUnvestedFunds(currEpoch, target)
 
 	st.LockedFunds = big.Sub(st.LockedFunds, amountUnlocked)
 	Assert(st.LockedFunds.GreaterThanEqual(big.Zero()))
@@ -904,7 +843,9 @@ func (st *State) UnlockVestedFunds(store adt.Store, currEpoch abi.ChainEpoch) (a
 		return big.Zero(), xerrors.Errorf("failed to load vesting funds: %w", err)
 	}
 
-	amountUnlocked := st.unlockVestedFunds(vestingFunds, currEpoch)
+	amountUnlocked := vestingFunds.unlockVestedFunds(currEpoch)
+	st.LockedFunds = big.Sub(st.LockedFunds, amountUnlocked)
+	Assert(st.LockedFunds.GreaterThanEqual(big.Zero()))
 
 	err = st.SaveVestingFunds(store, vestingFunds)
 	if err != nil {
@@ -912,30 +853,6 @@ func (st *State) UnlockVestedFunds(store adt.Store, currEpoch abi.ChainEpoch) (a
 	}
 
 	return amountUnlocked, nil
-}
-
-func (st *State) unlockVestedFunds(vestingFunds *VestingFunds, currEpoch abi.ChainEpoch) abi.TokenAmount {
-	amountUnlocked := abi.NewTokenAmount(0)
-
-	lastIndexToRemove := -1
-	for i, vf := range vestingFunds.Funds {
-		if vf.Epoch >= currEpoch {
-			break
-		}
-
-		amountUnlocked = big.Add(amountUnlocked, vf.Amount)
-		lastIndexToRemove = i
-	}
-
-	// remove all entries upto and including lastIndexToRemove
-	if lastIndexToRemove != -1 {
-		vestingFunds.Funds = vestingFunds.Funds[lastIndexToRemove+1:]
-	}
-
-	st.LockedFunds = big.Sub(st.LockedFunds, amountUnlocked)
-	Assert(st.LockedFunds.GreaterThanEqual(big.Zero()))
-
-	return amountUnlocked
 }
 
 // CheckVestedFunds returns the amount of vested funds that have vested before the provided epoch.
