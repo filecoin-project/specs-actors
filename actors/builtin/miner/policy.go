2342a6e2fa572d77e79b72419e1ec99cecab59db
ZenGround0 <5515260+ZenGround0@users.noreply.github.com>
2020-08-31 22:08:18 -0400

    Fix finality and epochs per year (#1062)
    Co-authored-by: ZenGround0 <ZenGround0@users.noreply.github.com>

--- actors/builtin/miner/policy.go
+++ actors/builtin/miner/policy.go
@@ -76,7 +76,7 @@ func loadPartitionsSectorsMax(partitionSectorCount uint64) uint64 {
 
 // Epochs after which chain state is final with overwhelming probability (hence the likelihood of two fork of this size is negligible)
 // This is a conservative value that is chosen via simulations of all known attacks.
-const ChainFinality = abi.ChainEpoch(1400) // PARAM_SPEC
+const ChainFinality = abi.ChainEpoch(900) // PARAM_SPEC
 
 // Prefix for sealed sector CIDs (CommR).
 var SealedCIDPrefix = cid.Prefix{
