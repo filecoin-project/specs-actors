ee785ca8bdf150b753ed41307d2649251ac7d62e
ZenGround0 <ZenGround0@users.noreply.github.com>
2020-09-09 10:21:45 -0400

    WIP

--- actors/builtin/miner/deadlines.go
+++ actors/builtin/miner/deadlines.go
@@ -4,110 +4,19 @@ import (
 	"errors"
 
 	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/deadline"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
 
-// Deadline calculations with respect to a current epoch.
-// "Deadline" refers to the window during which proofs may be submitted.
-// Windows are non-overlapping ranges [Open, Close), but the challenge epoch for a window occurs before
-// the window opens.
-// The current epoch may not necessarily lie within the deadline or proving period represented here.
-type DeadlineInfo struct {
-	CurrentEpoch abi.ChainEpoch // Epoch at which this info was calculated.
-	PeriodStart  abi.ChainEpoch // First epoch of the proving period (<= CurrentEpoch).
-	Index        uint64         // A deadline index, in [0..WPoStProvingPeriodDeadlines) unless period elapsed.
-	Open         abi.ChainEpoch // First epoch from which a proof may be submitted (>= CurrentEpoch).
-	Close        abi.ChainEpoch // First epoch from which a proof may no longer be submitted (>= Open).
-	Challenge    abi.ChainEpoch // Epoch at which to sample the chain for challenge (< Open).
-	FaultCutoff  abi.ChainEpoch // First epoch at which a fault declaration is rejected (< Open).
-}
-
-// Whether the proving period has begun.
-func (d *DeadlineInfo) PeriodStarted() bool {
-	return d.CurrentEpoch >= d.PeriodStart
-}
-
-// Whether the proving period has elapsed.
-func (d *DeadlineInfo) PeriodElapsed() bool {
-	return d.CurrentEpoch >= d.NextPeriodStart()
-}
-
-// The last epoch in the proving period.
-func (d *DeadlineInfo) PeriodEnd() abi.ChainEpoch {
-	return d.PeriodStart + WPoStProvingPeriod - 1
-}
-
-// The first epoch in the next proving period.
-func (d *DeadlineInfo) NextPeriodStart() abi.ChainEpoch {
-	return d.PeriodStart + WPoStProvingPeriod
-}
-
-// Whether the current deadline is currently open.
-func (d *DeadlineInfo) IsOpen() bool {
-	return d.CurrentEpoch >= d.Open && d.CurrentEpoch < d.Close
-}
-
-// Whether the current deadline has already closed.
-func (d *DeadlineInfo) HasElapsed() bool {
-	return d.CurrentEpoch >= d.Close
-}
-
-// The last epoch during which a proof may be submitted.
-func (d *DeadlineInfo) Last() abi.ChainEpoch {
-	return d.Close - 1
-}
-
-// Epoch at which the subsequent deadline opens.
-func (d *DeadlineInfo) NextOpen() abi.ChainEpoch {
-	return d.Close
-}
-
-// Whether the deadline's fault cutoff has passed.
-func (d *DeadlineInfo) FaultCutoffPassed() bool {
-	return d.CurrentEpoch >= d.FaultCutoff
-}
-
-// Returns the next instance of this deadline that has not yet elapsed.
-func (d *DeadlineInfo) NextNotElapsed() *DeadlineInfo {
-	next := d
-	for next.HasElapsed() {
-		next = NewDeadlineInfo(next.NextPeriodStart(), d.Index, d.CurrentEpoch)
-	}
-	return next
-}
-
-func (d *DeadlineInfo) QuantSpec() QuantSpec {
-	return NewQuantSpec(WPoStProvingPeriod, d.Last())
-}
-
 // Returns deadline-related calculations for a deadline in some proving period and the current epoch.
-func NewDeadlineInfo(periodStart abi.ChainEpoch, deadlineIdx uint64, currEpoch abi.ChainEpoch) *DeadlineInfo {
-	if deadlineIdx < WPoStPeriodDeadlines {
-		deadlineOpen := periodStart + (abi.ChainEpoch(deadlineIdx) * WPoStChallengeWindow)
-		return &DeadlineInfo{
-			CurrentEpoch: currEpoch,
-			PeriodStart:  periodStart,
-			Index:        deadlineIdx,
-			Open:         deadlineOpen,
-			Close:        deadlineOpen + WPoStChallengeWindow,
-			Challenge:    deadlineOpen - WPoStChallengeLookback,
-			FaultCutoff:  deadlineOpen - FaultDeclarationCutoff,
-		}
-	} else {
-		// Return deadline info for a no-duration deadline immediately after the last real one.
-		afterLastDeadline := periodStart + WPoStProvingPeriod
-		return &DeadlineInfo{
-			CurrentEpoch: currEpoch,
-			PeriodStart:  periodStart,
-			Index:        deadlineIdx,
-			Open:         afterLastDeadline,
-			Close:        afterLastDeadline,
-			Challenge:    afterLastDeadline,
-			FaultCutoff:  0,
-		}
-	}
+func NewDeadlineInfo(periodStart abi.ChainEpoch, deadlineIdx uint64, currEpoch abi.ChainEpoch) *deadline.DeadlineInfo {
+	return deadline.NewDeadlineInfo(periodStart, deadlineIdx, currEpoch, WPoStPeriodDeadlines, WPoStProvingPeriod, WPoStChallengeWindow, WPoStChallengeLookback, FaultDeclarationCutoff)
+}
+
+func QuantSpecForDeadline(di *deadline.DeadlineInfo) QuantSpec {
+	return NewQuantSpec(WPoStProvingPeriod, di.Last())
 }
 
 // FindSector returns the deadline and partition index for a sector number.
--- go.mod
+++ go.mod
@@ -7,7 +7,11 @@ require (
 	github.com/filecoin-project/go-amt-ipld/v2 v2.1.0
 	github.com/filecoin-project/go-bitfield v0.2.0
 	github.com/filecoin-project/go-hamt-ipld/v2 v2.0.0
+<<<<<<< HEAD
 	github.com/filecoin-project/go-state-types v0.0.0-20200909080127-001afaca718c
+=======
+	github.com/filecoin-project/go-state-types v0.0.0-20200909022925-fcf71d562458
+>>>>>>> WIP
 	github.com/filecoin-project/specs-actors v0.9.6
 	github.com/ipfs/go-block-format v0.0.2
 	github.com/ipfs/go-cid v0.0.7
