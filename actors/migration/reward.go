66bd90cefdd2dcc2e9871ff926278c9b7b9c868f
Alex North <445306+anorth@users.noreply.github.com>
2020-09-30 09:50:59 +1000

    Revert resetting reward actor baseline value, already happening on release/v0.9 at nv=3 (#1187)

--- actors/migration/reward.go
+++ actors/migration/reward.go
@@ -8,7 +8,6 @@ import (
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	cid "github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
-	"golang.org/x/xerrors"
 
 	reward2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/reward"
 	smoothing2 "github.com/filecoin-project/specs-actors/v2/actors/util/smoothing"
@@ -23,19 +22,6 @@ func (m *rewardMigrator) MigrateState(ctx context.Context, store cbor.IpldStore,
 		return cid.Undef, big.Zero(), err
 	}
 
-	// The baseline function initial value and growth rate are changed from v0.
-	// As an approximation to working out what the baseline value would be at the migration epoch
-	// had the baseline parameters been this way all along, this just sets the immediately value to the
-	// new (higher) initial value, essentially restarting its calculation.
-	// The baseline and realized cumsums, and effective network time, are not changed.
-	// This boils down to a step change in the baseline function, as if it had been defined piecewise.
-	// This will be a bit annoying for external analytical calculations of the baseline function.
-
-	if inState.ThisEpochBaselinePower.GreaterThan(reward2.BaselineInitialValue) {
-		return cid.Undef, big.Zero(), xerrors.Errorf("unexpected baseline power %v higher than new initial value %v",
-			inState.ThisEpochBaselinePower, reward2.BaselineInitialValue)
-	}
-
 	outState := reward2.State{
 		CumsumBaseline:          inState.CumsumBaseline,
 		CumsumRealized:          inState.CumsumRealized,
@@ -43,7 +29,7 @@ func (m *rewardMigrator) MigrateState(ctx context.Context, store cbor.IpldStore,
 		EffectiveBaselinePower:  inState.EffectiveBaselinePower,
 		ThisEpochReward:         inState.ThisEpochReward,
 		ThisEpochRewardSmoothed: smoothing2.FilterEstimate(*inState.ThisEpochRewardSmoothed),
-		ThisEpochBaselinePower:  reward2.BaselineInitialValue,
+		ThisEpochBaselinePower:  inState.ThisEpochBaselinePower,
 		Epoch:                   inState.Epoch,
 		TotalStoragePowerReward: inState.TotalMined,
 	}
