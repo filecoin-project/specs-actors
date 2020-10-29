e8f5b6dc1fb4618ca144a8adebc89c968d50042f
acruikshank <acruikshank@example.com>
2020-10-29 14:55:33 -0400

    add invariant checks

--- support/agent/cases_test.go
+++ support/agent/cases_test.go
@@ -6,9 +6,11 @@ import (
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/power"
+	"github.com/filecoin-project/specs-actors/v2/actors/states"
 	"github.com/filecoin-project/specs-actors/v2/support/agent"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"strings"
 	"testing"
 )
 
@@ -59,6 +61,16 @@ func TestCommitSectors(t *testing.T) {
 		require.NoError(t, sim.Tick())
 
 		if i%100 == 0 {
+			stateTree, err := sim.GetVM().GetStateTree()
+			require.NoError(t, err)
+
+			totalBalance, err := sim.GetVM().GetTotalActorBalance()
+			require.NoError(t, err)
+
+			acc, err := states.CheckStateInvariants(stateTree, totalBalance, sim.GetVM().GetEpoch())
+			require.NoError(t, err)
+			require.True(t, acc.IsEmpty(), strings.Join(acc.Messages(), "\n"))
+
 			require.NoError(t, sim.GetVM().GetState(builtin.StoragePowerActorAddr, &pwrSt))
 			fmt.Printf("Power at %d: raw: %v   qa: %v   cmtRaw: %v   cmtQa: %v   cnsMnrs: %d\n", sim.GetVM().GetEpoch(),
 				pwrSt.TotalRawBytePower, pwrSt.TotalQualityAdjPower, pwrSt.TotalBytesCommitted, pwrSt.TotalQABytesCommitted, pwrSt.MinerAboveMinPowerCount)
