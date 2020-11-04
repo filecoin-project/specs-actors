1316e95b64c7df22dabcaae40b6912d1036f62d7
acruikshank <acruikshank@example.com>
2020-11-04 16:59:42 -0500

    revert interface narrowing in address test support

--- support/testing/address.go
+++ support/testing/address.go
@@ -4,22 +4,23 @@ import (
 	addr "github.com/filecoin-project/go-address"
 	"github.com/stretchr/testify/require"
 	"math/rand"
+	"testing"
 )
 
-func NewIDAddr(t require.TestingT, id uint64) addr.Address {
+func NewIDAddr(t testing.TB, id uint64) addr.Address {
 	address, err := addr.NewIDAddress(id)
 	require.NoError(t, err)
 	return address
 }
 
-func NewSECP256K1Addr(t require.TestingT, pubkey string) addr.Address {
+func NewSECP256K1Addr(t testing.TB, pubkey string) addr.Address {
 	// the pubkey of a secp256k1 address is hashed for consistent length.
 	address, err := addr.NewSecp256k1Address([]byte(pubkey))
 	require.NoError(t, err)
 	return address
 }
 
-func NewBLSAddr(t require.TestingT, seed int64) addr.Address {
+func NewBLSAddr(t testing.TB, seed int64) addr.Address {
 	// the pubkey of a bls address is not hashed and must be the correct length.
 	buf := make([]byte, 48)
 	r := rand.New(rand.NewSource(seed))
@@ -30,7 +31,7 @@ func NewBLSAddr(t require.TestingT, seed int64) addr.Address {
 	return address
 }
 
-func NewActorAddr(t require.TestingT, data string) addr.Address {
+func NewActorAddr(t testing.TB, data string) addr.Address {
 	address, err := addr.NewActorAddress([]byte(data))
 	require.NoError(t, err)
 	return address
