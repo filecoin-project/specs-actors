8599bd2e7371500c326867bd735ddd05bd6d548f
zgfzgf <1901989065@qq.com>
2020-11-09 09:14:17 +0800

    EpochsInFiveYears replaces fiveYears

--- actors/builtin/sector.go
+++ actors/builtin/sector.go
@@ -37,24 +37,24 @@ var SealProofPolicies = map[stabi.RegisteredSealProof]*SealProofPolicy{
 	},
 
 	stabi.RegisteredSealProof_StackedDrg2KiBV1_1: {
-		SectorMaxLifetime:          fiveYears,
-		ConsensusMinerMinPower:     stabi.NewStoragePower(0),
+		SectorMaxLifetime:      EpochsInFiveYears,
+		ConsensusMinerMinPower: stabi.NewStoragePower(0),
 	},
 	stabi.RegisteredSealProof_StackedDrg8MiBV1_1: {
-		SectorMaxLifetime:          fiveYears,
-		ConsensusMinerMinPower:     stabi.NewStoragePower(16 << 20),
+		SectorMaxLifetime:      EpochsInFiveYears,
+		ConsensusMinerMinPower: stabi.NewStoragePower(16 << 20),
 	},
 	stabi.RegisteredSealProof_StackedDrg512MiBV1_1: {
-		SectorMaxLifetime:          fiveYears,
-		ConsensusMinerMinPower:     stabi.NewStoragePower(1 << 30),
+		SectorMaxLifetime:      EpochsInFiveYears,
+		ConsensusMinerMinPower: stabi.NewStoragePower(1 << 30),
 	},
 	stabi.RegisteredSealProof_StackedDrg32GiBV1_1: {
-		SectorMaxLifetime:          fiveYears,
-		ConsensusMinerMinPower:     stabi.NewStoragePower(10 << 40),
+		SectorMaxLifetime:      EpochsInFiveYears,
+		ConsensusMinerMinPower: stabi.NewStoragePower(10 << 40),
 	},
 	stabi.RegisteredSealProof_StackedDrg64GiBV1_1: {
-		SectorMaxLifetime:          fiveYears,
-		ConsensusMinerMinPower:     stabi.NewStoragePower(20 << 40),
+		SectorMaxLifetime:      EpochsInFiveYears,
+		ConsensusMinerMinPower: stabi.NewStoragePower(20 << 40),
 	},
 }
 
