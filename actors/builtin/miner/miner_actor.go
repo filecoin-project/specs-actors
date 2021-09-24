6b069df6b603429f9b839a42e90694ec147cce36
ZenGround0 <ZenGround0@users.noreply.github.com>
2021-09-24 13:44:55 -0400

    Precommit batch pays aggregate fee

--- actors/builtin/miner/miner_actor.go
+++ actors/builtin/miner/miner_actor.go
@@ -752,6 +752,14 @@ func (a Actor) PreCommitSectorBatch(rt Runtime, params *PreCommitSectorBatchPara
 	feeToBurn := abi.NewTokenAmount(0)
 	var needsCron bool
 	rt.StateTransaction(&st, func() {
+		// Aggregate fee applies only when batching.
+		if len(params.Sectors) > 1 {
+			aggregateFee := AggregatePreCommitNetworkFee(len(params.Sectors), rt.BaseFee())
+			// AggregateFee applied to fee debt to consolidate burn with outstanding debts
+			err := st.ApplyPenalty(aggregateFee)
+			builtin.RequireNoErr(rt, err, exitcode.ErrIllegalState, "failed to apply penalty")
+		}
+
 		// available balance already accounts for fee debt so it is correct to call
 		// this before RepayDebts. We would have to
 		// subtract fee debt explicitly if we called this after.
