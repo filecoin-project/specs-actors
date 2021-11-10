a99d178a61f95dbf3e3f3c593c360c438d9a55fc
Claudia Richoux <c@laudiacay.cool>
2021-11-10 18:13:03 +0000

    modulo claims she's ALIVE

--- actors/migration/nv14/test/test_miner_type_removal_test.go
+++ actors/migration/nv14/test/test_miner_type_removal_test.go
@@ -12,8 +12,10 @@ import (
 	"github.com/stretchr/testify/require"
 
 	ipld2 "github.com/filecoin-project/specs-actors/v2/support/ipld"
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	power4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/power"
 	vm4 "github.com/filecoin-project/specs-actors/v4/support/vm"
+	vm5 "github.com/filecoin-project/specs-actors/v5/support/vm"
 
 	"github.com/filecoin-project/specs-actors/v5/actors/migration/nv13"
 	"github.com/filecoin-project/specs-actors/v6/actors/builtin"
@@ -44,19 +46,24 @@ func TestTestMinerTypeDeletion(t *testing.T) {
 	ctx := context.Background()
 	log := nv14.TestLogger{TB: t}
 	bs := ipld2.NewSyncBlockStoreInMemory()
-	v := vm4.NewVMWithSingletons(ctx, t, bs)
+	v4 := vm4.NewVMWithSingletons(ctx, t, bs)
 	adtStore := adt.WrapStore(ctx, cbor.NewCborStore(bs))
 
-	addrs := vm4.CreateAccounts(ctx, t, v, 3, big.Mul(big.NewInt(10_000), vm.FIL), 93837778)
+	addrs := vm4.CreateAccounts(ctx, t, v4, 3, big.Mul(big.NewInt(10_000), vm.FIL), 93837778)
 	worker1, worker2, minerOwner := addrs[0], addrs[1], addrs[2]
 
 	minerBalance := big.Mul(big.NewInt(1_000), vm.FIL)
 
 	// getting the market actor balance here for CheckStateInvariants later down
-	market4Actor, found, err := v.GetActor(builtin.StorageMarketActorAddr)
+	market4Actor, found, err := v4.GetActor(builtin4.StorageMarketActorAddr)
 	require.NoError(t, err)
 	require.True(t, found)
 	oldMarketActorBalance := market4Actor.Balance
+
+	ownerActor, found, err := v4.GetActor(minerOwner)
+	require.NoError(t, err)
+	require.True(t, found)
+	oldOwnerBalance := ownerActor.Balance
 
 	// create a miner with the wrong post/seal proof type
 	params := power4.CreateMinerParams{
@@ -65,9 +72,9 @@ func TestTestMinerTypeDeletion(t *testing.T) {
 		WindowPoStProofType: abi.RegisteredPoStProof_StackedDrgWindow8MiBV1,
 		Peer:                abi.PeerID("not really a peer id"),
 	}
-	ret := vm4.ApplyOk(t, v, addrs[0], builtin.StoragePowerActorAddr, minerBalance, builtin.MethodsPower.CreateMiner, &params)
+	ret := vm4.ApplyOk(t, v4, addrs[0], builtin4.StoragePowerActorAddr, minerBalance, builtin4.MethodsPower.CreateMiner, &params)
 
-	minerAddrs1, ok := ret.(*power.CreateMinerReturn)
+	testMinerTypeAddr, ok := ret.(*power4.CreateMinerReturn)
 	require.True(t, ok)
 
 	params = power4.CreateMinerParams{
@@ -76,46 +83,57 @@ func TestTestMinerTypeDeletion(t *testing.T) {
 		WindowPoStProofType: abi.RegisteredPoStProof_StackedDrgWindow32GiBV1,
 		Peer:                abi.PeerID("not really another peer id"),
 	}
-	ret = vm4.ApplyOk(t, v, addrs[0], builtin.StoragePowerActorAddr, minerBalance, builtin.MethodsPower.CreateMiner, &params)
+	ret = vm4.ApplyOk(t, v4, addrs[0], builtin4.StoragePowerActorAddr, minerBalance, builtin4.MethodsPower.CreateMiner, &params)
 
-	minerAddrs2, ok := ret.(*power.CreateMinerReturn)
+	realMinerTypeAddr, ok := ret.(*power4.CreateMinerReturn)
 	require.True(t, ok)
 
 	// add market collateral for miners
 	collateral := big.Mul(big.NewInt(64), vm.FIL)
-	vm4.ApplyOk(t, v, worker1, builtin.StorageMarketActorAddr, collateral, builtin.MethodsMarket.AddBalance, &minerAddrs1.IDAddress)
-	vm4.ApplyOk(t, v, worker2, builtin.StorageMarketActorAddr, collateral, builtin.MethodsMarket.AddBalance, &minerAddrs2.IDAddress)
+	vm4.ApplyOk(t, v4, worker1, builtin4.StorageMarketActorAddr, collateral, builtin4.MethodsMarket.AddBalance, &testMinerTypeAddr.IDAddress)
+	vm4.ApplyOk(t, v4, worker2, builtin4.StorageMarketActorAddr, collateral, builtin4.MethodsMarket.AddBalance, &realMinerTypeAddr.IDAddress)
 
-	// do the migration, it should delete one of the miners and transfer its escrow and balance back to the owner
-	startRoot := v.StateRoot()
-	nextRoot, err := nv13.MigrateStateTree(ctx, adtStore, startRoot, abi.ChainEpoch(0),
-		nv13.Config{MaxWorkers: 1}, log, nv13.NewMemMigrationCache())
+	totalPreviousRuntimeBalance, err := v4.GetTotalActorBalance()
 	require.NoError(t, err)
 
-	nextNextRoot, err := nv14.MigrateStateTree(ctx, adtStore, nextRoot, abi.ChainEpoch(0),
-		nv14.Config{MaxWorkers: 1}, log, nv14.NewMemMigrationCache())
+	// do the migration, it should delete one of the miners and transfer its escrow and balance back to the owner
+	v4startRoot := v4.StateRoot()
+	v5nextRoot, err := nv13.MigrateStateTree(ctx, adtStore, v4startRoot, abi.ChainEpoch(0),
+		nv13.Config{MaxWorkers: 1}, log, nv13.NewMemMigrationCache())
 	require.NoError(t, err)
 
 	lookup := map[cid.Cid]rt.VMActor{}
 	for _, ba := range exported.BuiltinActors() {
 		lookup[ba.Code()] = ba
 	}
+	v5, err := vm5.NewVMAtEpoch(ctx, lookup, v4.Store(), v5nextRoot, v4.GetEpoch()+1)
+	require.NoError(t, err)
+	v5startRoot := v5.StateRoot()
 
-	v6, err := vm.NewVMAtEpoch(ctx, lookup, v.Store(), nextNextRoot, v.GetEpoch()+1)
+	v6nextRoot, err := nv14.MigrateStateTree(ctx, adtStore, v5startRoot, abi.ChainEpoch(0),
+		nv14.Config{MaxWorkers: 1}, log, nv14.NewMemMigrationCache())
+	require.NoError(t, err)
+
+	lookup = map[cid.Cid]rt.VMActor{}
+	for _, ba := range exported.BuiltinActors() {
+		lookup[ba.Code()] = ba
+	}
+
+	v6, err := vm.NewVMAtEpoch(ctx, lookup, v5.Store(), v6nextRoot, v5.GetEpoch()+1)
 	require.NoError(t, err)
 
 	// check the market state after migration
 	var market6State market.State
 	require.NoError(t, v6.GetState(builtin.StorageMarketActorAddr, &market6State))
-	market.CheckStateInvariants(&market6State, v6.Store(), oldMarketActorBalance, v.GetEpoch()+1)
+	market.CheckStateInvariants(&market6State, v6.Store(), oldMarketActorBalance, v6.GetEpoch()+1)
 
 	// check both miners' escrow balances (should be zero and collateral, respectively)
 	et, err := adt.AsBalanceTable(adtStore, market6State.EscrowTable)
 	require.NoError(t, err)
-	bal, err := et.Get(minerAddrs1.IDAddress)
+	bal, err := et.Get(testMinerTypeAddr.IDAddress)
 	require.NoError(t, err)
 	require.Equal(t, bal, big.Zero())
-	bal, err = et.Get(minerAddrs2.IDAddress)
+	bal, err = et.Get(realMinerTypeAddr.IDAddress)
 	require.NoError(t, err)
 	require.Equal(t, bal, collateral)
 
@@ -123,31 +141,38 @@ func TestTestMinerTypeDeletion(t *testing.T) {
 	var power6State power.State
 	require.NoError(t, v6.GetState(builtin.StoragePowerActorAddr, &power6State))
 	power.CheckStateInvariants(&power6State, v6.Store())
-	require.Equal(t, power6State.MinerAboveMinPowerCount, 1)
-	require.Equal(t, power6State.MinerCount, 1)
+	require.Equal(t, power6State.MinerCount, int64(1))
+	// maybe should make one or both of these miners above
+	require.Equal(t, power6State.MinerAboveMinPowerCount, int64(0))
 
-	// XXX: there shouldn't be any claims period for the test miner type, so i shouldn't need to worry about those?
-	// .... so we hope?
+	// XXX: check claims removal here
 
 	// check that the total amount of filecoin stayed constant!
-	totalRuntimeBalance, err := v6.GetTotalActorBalance()
+	totalCurrentRuntimeBalance, err := v6.GetTotalActorBalance()
 	require.NoError(t, err)
-	require.Equal(t, totalRuntimeBalance, big.Mul(big.NewInt(2), minerBalance))
+	require.Equal(t, totalPreviousRuntimeBalance, totalCurrentRuntimeBalance)
 
 	// check that the test miner was properly deleted
-	_, deletedMinerFound, err := v6.GetActor(minerAddrs1.IDAddress)
+	_, deletedMinerFound, err := v6.GetActor(testMinerTypeAddr.IDAddress)
 	require.NoError(t, err)
 	require.False(t, deletedMinerFound)
 
 	// check that the non-test miner was not deleted and still has its remaining money
-	nonTestMiner, found, err := v6.GetActor(minerAddrs2.IDAddress)
+	nonTestMiner, found, err := v6.GetActor(realMinerTypeAddr.IDAddress)
 	require.NoError(t, err)
 	require.True(t, found)
-	require.Equal(t, nonTestMiner.Balance, big.Sub(minerBalance, collateral))
+	require.Equal(t, nonTestMiner.Balance, minerBalance)
+
+	marketActor, found, err := v6.GetActor(builtin.StorageMarketActorAddr)
+	require.NoError(t, err)
+	require.True(t, found)
+	require.Equal(t, marketActor.Balance, collateral)
 
 	// check the owner's new balance and make sure it's same as the miner1 balance
-	ownerActor, found, err := v6.GetActor(minerOwner)
+	ownerActor, found, err = v6.GetActor(minerOwner)
 	require.NoError(t, err)
 	require.True(t, found)
-	require.Equal(t, ownerActor.Balance, minerBalance)
+	require.Equal(t,
+		big.Add(oldOwnerBalance, big.Add(minerBalance, collateral)),
+		ownerActor.Balance)
 }
