fe9bca74d5e5cbb29f7f00f652c4ae8a64e27adb
vyzo <vyzo@hackzen.org>
2022-04-01 19:15:14 +0300

    make gen

--- actors/builtin/system/cbor_gen.go
+++ actors/builtin/system/cbor_gen.go
@@ -12,7 +12,7 @@ import (
 
 var _ = xerrors.Errorf
 
-var lengthBufState = []byte{128}
+var lengthBufState = []byte{129}
 
 func (t *State) MarshalCBOR(w io.Writer) error {
 	if t == nil {
@@ -21,6 +21,14 @@ func (t *State) MarshalCBOR(w io.Writer) error {
 	}
 	if _, err := w.Write(lengthBufState); err != nil {
 		return err
+	}
+
+	scratch := make([]byte, 9)
+
+	// t.BuiltinActors (cid.Cid) (struct)
+
+	if err := cbg.WriteCidBuf(scratch, w, t.BuiltinActors); err != nil {
+		return xerrors.Errorf("failed to write cid field t.BuiltinActors: %w", err)
 	}
 
 	return nil
@@ -40,9 +48,21 @@ func (t *State) UnmarshalCBOR(r io.Reader) error {
 		return fmt.Errorf("cbor input should be of type array")
 	}
 
-	if extra != 0 {
+	if extra != 1 {
 		return fmt.Errorf("cbor input had wrong number of fields")
 	}
 
+	// t.BuiltinActors (cid.Cid) (struct)
+
+	{
+
+		c, err := cbg.ReadCid(br)
+		if err != nil {
+			return xerrors.Errorf("failed to read cid field t.BuiltinActors: %w", err)
+		}
+
+		t.BuiltinActors = c
+
+	}
 	return nil
 }
