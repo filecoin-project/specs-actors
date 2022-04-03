ffbe3c81f688ea1571c28f6eb4962b501fe8983e
vyzo <vyzo@hackzen.org>
2022-04-03 14:41:16 +0300

    normalize system actor state construction

--- actors/builtin/system/system_actor.go
+++ actors/builtin/system/system_actor.go
@@ -3,11 +3,12 @@ package system
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
+	"github.com/filecoin-project/go-state-types/exitcode"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/specs-actors/v8/actors/builtin"
-	"github.com/filecoin-project/specs-actors/v8/actors/builtin/manifest"
 	"github.com/filecoin-project/specs-actors/v8/actors/runtime"
+	"github.com/filecoin-project/specs-actors/v8/actors/util/adt"
 )
 
 type Actor struct{}
@@ -34,12 +35,8 @@ var _ runtime.VMActor = Actor{}
 
 func (a Actor) Constructor(rt runtime.Runtime, _ *abi.EmptyValue) *abi.EmptyValue {
 	rt.ValidateImmediateCallerIs(builtin.SystemActorAddr)
-
-	empty := rt.StorePut(&manifest.ManifestData{})
-	rt.StateCreate(&State{BuiltinActors: empty})
+	st, err := ConstructState(adt.AsStore(rt))
+	builtin.RequireNoErr(rt, err, exitcode.ErrIllegalState, "failed to construct state")
+	rt.StateCreate(st)
 	return nil
-}
-
-type State struct {
-	BuiltinActors cid.Cid // ManifestData
 }
